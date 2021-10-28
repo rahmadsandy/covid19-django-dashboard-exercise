4bca13445e1b7ff86d3c76e8e1c2fc4deefd5f72
production <production@localhost.localdomain>
2021-10-28 11:06:17 +0700

    add maps.py

--- processdata/views.py
+++ processdata/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+
+def index(request):
+    return render(request, template_name='index.html')
